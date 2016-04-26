2bd13eb17f43ca67a89ecd897649b6db117a4f42
Arne Küderle <a.kuederle@gmail.com>
2016-04-26 19:49:16 +0200

    added custom lockscreen

---
+++ custom_scripts/lock_screen.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+exec xautolock \
+  -time 5 -locker "i3lock -i ~/Pictures/Backgrounds/main_background.png -e -f --insidevercolor=268bd2bf --insidewrongcolor=dc322fbf --insidecolor=073642bf --ringvercolor=268bd2ff --ringwrongcolor=dc322fff --ringcolor=073642ff --textcolor=839496ff --separatorcolor=073642ff --keyhlcolor=2aa198ff --bshlcolor=cb4b16ff -r" \
+  -notify 30 \
+  -notifier "notify-send -t 30000 -- 'LOCKING screen soon'" \
+  -corners ----
